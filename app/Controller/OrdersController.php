7ed6a4b7f67350ce449cd32bf12bcbc3e5410425
Alex Kwan <alexkwan089@gmail.com>
2012-10-15 03:19:10 -0700

    added some css to the site

--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -3,18 +3,21 @@
 class OrdersController extends AppController {
 	
 	var $name = 'Orders';
+    var $layout = 'default';// dont need to specify if using default
 	
     public $helpers = array('Html', 'Form', 'Session');
     public $components = array('Session');
 
     public function index() {
         $this->set('orders', $this->Order->find('all'));
+        $this->set('title_for_layout', 'Current Orders');
     }
 	
 	
     public function view($id = null) {
         $this->Order->id = $id;
         $this->set('order', $this->Order->read());
+        $this->set('title_for_layout', 'Current Order'.$id);
     }
     
     public function delete($id = null ) {
@@ -23,6 +26,7 @@ class OrdersController extends AppController {
         $this->redirect(array('action'=>'index'));
     }
     public function add() {
+        $this->set('title_for_layout', 'Add Order');
     	if ($this->request->is('post')) {
     		$this->Order->create();
     		if ($this->Order->save($this->request->data)) {
