98156443e56df4ea15f5af6a0d06caa30c3b9290
Alex Kwan <alexkwan089@gmail.com>
2012-10-16 14:52:27 -0700

    removed wei's first commit haha

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -3,7 +3,7 @@
 class Order extends AppModel {
 	
 	public $validate = array(
-	// wei was here
+	
 			'customerName' => array(
 					'rule' => 'notEmpty',
 					'message'=> 'Please enter the customers name'
